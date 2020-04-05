6881862c462ebf34ece950b618705eec08d87300
TheNote <38653750+note3crafter@users.noreply.github.com>
2020-04-05 13:02:46 +0200

    Sollte nun gehen...

--- src/TheNote/core/Main.php
+++ src/TheNote/core/Main.php
@@ -33,22 +33,22 @@ class Main extends PluginBase {
 
     public function onEnable(){
         self::$instance = $this;
-        @mkdir($this->getDataFolder());
+        @mkdir($this->getDataFolder() . "players/");
 
-		$this->getServer()->getPluginManager()->registerEvents(new MainListener($this), $this);
+	$this->getServer()->getPluginManager()->registerEvents(new MainListener($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new JoinEvent($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new QuitEvent($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new VanishEvent($this), $this);
         $this->getServer()->getCommandMap()->register("payall", new PayallCommand($this));
         $this->getServer()->getCommandMap()->register("nick", new NickCommand($this));
         $this->getServer()->getCommandMap()->register("day", new DayCommand($this));
-		$this->getServer()->getCommandMap()->register("night", new NightCommand($this));
-		$this->getServer()->getCommandMap()->register("feed", new FeedCommand($this));
-		$this->getServer()->getCommandMap()->register("heal", new HealCommand($this));
-		$this->getServer()->getCommandMap()->register("gms", new SurvivalCommand($this));
-		$this->getServer()->getCommandMap()->register("gmc", new KreativCommand($this));
-		$this->getServer()->getCommandMap()->register("gma", new AbenteuerCommand($this));
-		$this->getServer()->getCommandMap()->register("gmspc", new ZuschauerCommand($this));
+	$this->getServer()->getCommandMap()->register("night", new NightCommand($this));
+	$this->getServer()->getCommandMap()->register("feed", new FeedCommand($this));
+	$this->getServer()->getCommandMap()->register("heal", new HealCommand($this));
+	$this->getServer()->getCommandMap()->register("gms", new SurvivalCommand($this));
+	$this->getServer()->getCommandMap()->register("gmc", new KreativCommand($this));
+	$this->getServer()->getCommandMap()->register("gma", new AbenteuerCommand($this));
+	$this->getServer()->getCommandMap()->register("gmspc", new ZuschauerCommand($this));
         $this->getServer()->getCommandMap()->register("chatclear", new ChatClearCommand($this));
         $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
         $this->getServer()->getCommandMap()->register("vanish", new VanishCommand($this));
